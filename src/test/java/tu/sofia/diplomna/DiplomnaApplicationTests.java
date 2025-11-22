87196c7ef78c8cbe771e5d45b0a98401c1dd774e
Damyan <damyan1mihov@gmail.com>
2025-11-22 13:12:45 +0200

    Initial commit

---
+++ src/test/java/tu/sofia/diplomna/DiplomnaApplicationTests.java
@@ -0,0 +1,13 @@
+package tu.sofia.diplomna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
